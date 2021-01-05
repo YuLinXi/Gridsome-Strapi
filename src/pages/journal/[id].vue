5a18b1bdb0f161794d87766d47f9431b493d026e
YuLinXi <yumengyuan@klicen.com>
2021-01-05 17:57:01 +0800

    :construction: journal post details

---
+++ src/pages/journal/[id].vue
@@ -0,0 +1,60 @@
+<template>
+  <layout>
+    <div class="journal">
+      <div class="container journal-container">
+        <div class="journal-header">
+          <h1 class="journal-title">VueJS for your next project</h1>
+          <div class="journal-meta">
+            <div class="journal-author">
+              <span class="label">Author</span>
+              <span class="author-name">Nichlas W. Andersen</span>
+            </div>
+            <div class="journal-date">
+              <span class="label">Date</span>
+              <div>4. June 2019</div>
+            </div>
+            <div class="journal-time">
+              <span class="label">Time</span>
+              <span>1 min read</span>
+            </div>
+          </div>
+        </div>
+        <div
+          class="journal-content"
+        >
+        </div>
+      </div>
+    </div>
+  </layout>
+</template>
+
+<script>
+export default {
+  name: "journal_id"
+}
+</script>
+
+<style scoped lang="less">
+  .journal-container {
+    max-width: 840px;
+  }
+  .journal-header {
+    padding: 2rem 0 4rem;
+  }
+  .journal-title {
+    font-size: 4rem;
+    margin: 0 0 4rem;
+    padding: 0;
+  }
+  .journal-meta {
+    display: flex;
+    flex-wrap: wrap;
+    font-size: .8rem;
+    > div {
+      margin-right: 4rem;
+    }
+  }
+  .journal-author {
+
+  }
+</style>
---
+++ src/pages/journal/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <layout>
+    <div class="container">
+      <div class="journal-hero">
+        <h1 class="journal-header">
+          a wise person once said...
+        </h1>
+      </div>
+    </div>
+    <g-link to="/journal/123/" class="journal-post">
+      <div class="container journal">
+        <h2 class="journal-title">
+          Gridsome with Forestry CMS
+        </h2>
+        <p class="journal-excerpt">
+          Get up and running with Forestry CMS, a git-backed headless CMS with a slick editing interface.
+        </p>
+      </div>
+    </g-link>
+  </layout>
+</template>
+
+<script>
+export default {
+  name: "Journal"
+}
+</script>
+
+<style scoped lang="less">
+  .journal-hero {
+    padding: 4rem 0;
+    text-align: center;
+    color: var(--color-base-1);
+  }
+  .journal-header {
+    font-size: 3rem;
+    font-weight: 700;
+    padding: 0;
+    margin: 0;
+  }
+  .journal-post {
+    display: block;
+    padding: 2rem 0;
+    text-decoration: none;
+    transition: background .5s ease;
+    &:hover {
+      background-color: var(--color-base-1);
+      > .journal {
+        transform: translateX(4rem);
+      }
+    }
+  }
+  .container.journal {
+    max-width: 720px;
+  }
+  .journal-post > .journal {
+    transition: transform .5s ease;
+  }
+  .journal-post h1,
+  .journal-post h2 {
+    margin: 0;
+    padding: 0;
+  }
+  .journal-title {
+    font-size: 2rem;
+    color: var(--color-contrast);
+  }
+
+  @media (min-width: 560px) {
+    .journal-post{
+      padding: 3rem 0;
+    }
+  }
+
+  @media (min-width: 860px) {
+    .journal-post {
+      padding: 5rem 0;
+    }
+  }
+</style>
